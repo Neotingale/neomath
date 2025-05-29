d522238c852b4c8950d674eafda0445158c15b9f
Neotingale <manuelrodri117@gmail.com>
2025-05-28 19:42:06 -0600

    Renamed project to NeoMath in `setup.py` and updated author and repository metadata.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-	name='neoutils',
+	name='neomath',
 	version='1.1.0',
 	description='Librería personal para la materia de Métodos Numéricos',
 	long_description=open('README.md').read(),
 	long_description_content_type='text/markdown',
-	author='Manuel Rodriguez',
+	author='Neotingale',
 	author_email='alu.23130570@correo.itlalaguna.edu.mx',
-	url='https://github.com/Neotingale/neoutils',
+	url='https://github.com/Neotingale/neomath',
 	packages=find_packages(),
 	install_requires=[
 		'prettytable>=3.14.0',

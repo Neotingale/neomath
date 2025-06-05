9e365c9e4d42dd68c656b206810531054eaf92a8
Neotingale <manuelrodri117@gmail.com>
2025-06-05 16:51:45 -0600

    Refactored `r_squared` in `metrics.py` for improved readability and performance with `zip`.

--- statistics/metrics.py
+++ statistics/metrics.py
@@ -1,5 +1,8 @@
+
 def r_squared(y_true : list[float], y_pred : list[float]) -> float:
+	"""
+	"""
 	mean_y = sum(y_true) / len(y_true)
 	ss_tot = sum((yi - mean_y) ** 2 for yi in y_true)
-	ss_res = sum((y_true[i] - y_pred[i]) ** 2 for i in range(len(y_true)))
-	return 1 - (ss_res / ss_tot)
+	ss_res = sum((yi - ypi) ** 2 for yi, ypi in zip(y_true, y_pred))
+	return 1 - ss_res / ss_tot

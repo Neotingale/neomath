2e759540d2ae0beb1cd848446c53296a8705ae22
Neotingale <manuelrodri117@gmail.com>
2025-06-05 16:50:49 -0600

    Expanded `__all__` in `numerics/__init__.py` to include solvers and improved import structure.

--- numerics/__init__.py
+++ numerics/__init__.py
@@ -1,5 +1,12 @@
-from polynomials import evaluate_polynomial
+from polynomials import (
+	evaluate_polynomial
+)
+from solvers import (
+	bisection, secant, gauss_seidel, gauss_jordan, false_position, newton_raphson,
+	jacobi, fixed_point
+)
 
 __all__ : [
-	evaluate_polynomial
+	evaluate_polynomial, bisection, secant, gauss_seidel, gauss_jordan,
+	false_position, newton_raphson, jacobi, fixed_point
 ]

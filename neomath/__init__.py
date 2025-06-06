bb7c031161d475cdf5be69cc54bd55d8093a3f50
Neotingale <manuelrodri117@gmail.com>
2025-06-05 18:16:11 -0600

    Added `__init__.py` to `neomath` namespace, consolidating imports and extending `__all__` for improved module accessibility.

---
+++ neomath/__init__.py
@@ -0,0 +1,15 @@
+from neomath.core import (Add, Mul, Pow, Symbol, expr, functions, symbols,)
+from neomath.numerics import (bisection, evaluate_polynomial, false_position,
+                              fixed_point, gauss_jordan, gauss_seidel, jacobi,
+                              newton_raphson, polynomials, secant, solvers,)
+from neomath.statistics import (linear_regression, metrics,
+                                polynomial_regression, r_squared, regression,)
+from neomath.utils import (parabola_vertex, vertex,)
+
+__all__ = ['Add', 'Mul', 'Pow', 'Symbol', 'algebra', 'bisection', 'constants',
+           'core', 'evaluate_polynomial', 'expr', 'false_position',
+           'fixed_point', 'functions', 'gauss_jordan', 'gauss_seidel',
+           'jacobi', 'linear_regression', 'metrics', 'newton_raphson',
+           'numerics', 'parabola_vertex', 'polynomial_regression',
+           'polynomials', 'r_squared', 'regression', 'secant', 'series',
+           'solvers', 'statistics', 'symbols', 'utils', 'vertex']
